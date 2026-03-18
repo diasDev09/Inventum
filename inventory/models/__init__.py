330e4fb5846f9d3a291ab9122cb1cf75d88ff5d1
Caio Dias <caio.mdias3@gmail.com>
2026-03-18 11:22:54 -0300

    implementa sistema de estoque
    Adiciona models de Produto, Categoria, Estoque e Movimentação com regras de entrada e saída

---
+++ inventory/models/__init__.py
@@ -0,0 +1,4 @@
+from .produto import *
+from .categoria import *
+from .estoque import *
+from .movimentacao import *
---
+++ inventory/models/movimentacao.py
@@ -0,0 +1,29 @@
+from django.db import models
+from .produto import Produto
+from .estoque import Estoque
+
+class Movimentacao(models.Model):
+
+    TIPO_CHOICES = [
+        ('E', 'Entrada'),
+        ('S', 'Saída'),
+    ]
+
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
+    quantidade = models.IntegerField()
+    data = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        estoque, created = Estoque.objects.get_or_create(produto=self.produto)
+
+        if self.tipo == 'E':
+            estoque.adicionar(self.quantidade)
+
+        elif self.tipo == 'S':
+            estoque.remover(self.quantidade)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.produto.nome} - {self.get_tipo_display()} - {self.quantidade}"
