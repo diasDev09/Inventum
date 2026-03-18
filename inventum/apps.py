ceadbef4c057abc0d4f77e05efd02d021336fef4
Caio Dias <caio.mdias3@gmail.com>
2026-03-18 10:21:28 -0300

    Atualização de Semântica

--- inventum/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InventumConfig(AppConfig):
-    name = 'inventum'
--- stockflow/settings.py
+++ stockflow/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'inventum',
+    'inventory',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
